cebf7fee362c4e2ccfd6fe1f8453fa91d9c5d041
Jakob Pogacnik Souvent <jakob.pogacnik.s@gmail.com>
2021-12-20 11:57:23 +0100

    Vaje 11

--- 11-deli-in-vladaj/vaje/deli_in_vladaj.py
+++ 11-deli-in-vladaj/vaje/deli_in_vladaj.py
@@ -27,7 +27,30 @@
 #     [10, 0, 2, 4, 11, 5, 17, 15, 18]
 ###############################################################################
 
+def pivot(list, start, end):
+    smaller_index = start+1
+    bigger_index = end
+    pivot = list[start] #pivot index is smaller_index - 1
+    while smaller_index <= bigger_index:
+        if list[smaller_index] < pivot:
+            list[smaller_index-1], list[smaller_index] = list[smaller_index], list[smaller_index-1] # zamenjamo pivot z manjsim eltom na desni
+            smaller_index += 1
 
+        elif list[smaller_index] > pivot:
+            if list[bigger_index] > pivot:
+                bigger_index -= 1
+            
+            elif list[bigger_index] < pivot:
+                list[smaller_index], list[bigger_index] = list[bigger_index], list[smaller_index]
+    
+    return smaller_index - 1
+
+print("Pivot:")
+a = [10, 4, 5, 15, 11, 2, 17, 0, 18]
+print(pivot(a, 1, 7))
+b = [3, 2, 4, 5, 10, 11, 17, 15]
+pivot(b, 0, 1)
+print(b)
 ###############################################################################
 # V tabeli želimo poiskati vrednost k-tega elementa po velikosti.
 #
@@ -43,6 +66,23 @@
 # rešite brez da v celoti uredite tabelo [a].
 ###############################################################################
 
+def kth_element_subfun(list, k, start, end):
+    if start > end: return None
+
+    k_th = pivot(list,start,end)
+    if k_th == k:
+        return list[k]
+    elif k_th > k:
+        return kth_element_subfun(list,k,start,k_th-1)
+    elif k_th < k:
+        return kth_element_subfun(list,k,k_th+1,end)
+    
+def kth_element(list,k):
+    return kth_element_subfun(list,k,0,(len(list)-1))
+
+print("kth:")
+a = [10, 4, 5, 15, 11, 3, 17, 2, 18]
+print(kth_element(a,3))
 
 ###############################################################################
 # Tabelo a želimo urediti z algoritmom hitrega urejanja (quicksort).
@@ -59,6 +99,19 @@
 #     [2, 3, 4, 5, 10, 11, 15, 17, 18]
 ###############################################################################
 
+def quicksort_part(a, start, end):
+    if start <= end:
+        n = pivot(a,start,end)
+        quicksort_part(a,start,n-1)
+        quicksort_part(a,n+1,end)
+
+def quicksort(list):
+    quicksort_part(list,0,len(a)-1)
+
+print("Quicksort:")
+a = [10, 4, 5, 15, 11, 3, 17, 2, 18]
+quicksort(a)
+print(a)
 
 ###############################################################################
 # Če imamo dve urejeni tabeli, potem urejeno združeno tabelo dobimo tako, da
@@ -84,6 +137,33 @@
 ###############################################################################
 
 
+def merge(target, list_1, list_2):
+    index_1 = 0
+    index_2 = 0
+    target_index = 0
+    while target_index < len(target):
+        try:
+            a = list_1[index_1]
+            b = list_2[index_2]
+            if a <= b:
+                c = a
+                index_1 += 1
+            else:
+                c = b
+                index_2 += 1
+
+        except IndexError: #index_1 or index_2 out of range
+            try:
+                c = list_1[index_1]
+                index_1 += 1
+            except IndexError:
+                c = list_2[index_2]
+                index_2 += 1
+        
+        target[target_index] = c
+        target_index += 1
+
+
 ###############################################################################
 # Tabelo želimo urediti z zlivanjem (merge sort). Tabelo razdelimo na polovici,
 # ju rekurzivno uredimo in nato zlijemo z uporabo funkcije [zlij].
@@ -99,3 +179,24 @@
 #     >>> a
 #     [2, 3, 4, 5, 10, 11, 15, 17, 18]
 ###############################################################################
+
+def mergesort(list):
+    if len(list) > 1:
+        n = len(list)//2
+        a = []
+        b = []
+        for i in range(len(list)):
+            if i < n:
+                a.append(list[i])
+            else:
+                b.append(list[i])
+            list[i] = -1
+
+        merge(list, mergesort(a), mergesort(b))
+
+    return list
+
+print("Merge:")
+a = [10, 4, 5, 15, 11, 3, 17, 2, 18]
+mergesort(a)
+print(a)
